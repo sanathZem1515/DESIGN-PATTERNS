a5ad3f8c8b6f1e966e3454b0bd38b76168fefc87
sanathZem1515 <sanath.bajjuri@zemosolabs.com>
2021-07-16 17:29:44 +0530

    solid commit

---
+++ DESIGN PROJECTS/observer/src/Observer.java
@@ -0,0 +1,5 @@
+public interface Observer
+{
+    public void update(int runs, int wickets,
+                       float overs);
+}
---
+++ DESIGN PROJECTS/observer/src/Subject.java
@@ -0,0 +1,6 @@
+public interface Subject
+{
+    public void registerObserver(Observer o);
+    public void unregisterObserver(Observer o);
+    public void notifyObservers();
+}
